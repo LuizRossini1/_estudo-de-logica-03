6d3421ddf687b431524756e62a72a89db3cd23e1
LuizRossini <luiz.guirossini123@gmail.com>
2024-07-02 18:38:55 -0300

    classe Cachorro e Override

--- src/Desafio02/Cachorro.java
+++ src/Desafio02/Cachorro.java
@@ -1,2 +1,19 @@
-package Desafio02;public class Cachorro {
+package Desafio02;
+
+public class Cachorro extends Animal {
+    @Override
+    public void emitirSom() {
+        System.out.println("Au! Au!");
+    }
+
+    public void abanarRabo() {
+        System.out.println("Abandando o rabo");
+    }
+
+    public static void main(String[] args) {
+        Cachorro meuCachorro = new Cachorro();
+
+        meuCachorro.emitirSom();
+        meuCachorro.abanarRabo();
+    }
 }
