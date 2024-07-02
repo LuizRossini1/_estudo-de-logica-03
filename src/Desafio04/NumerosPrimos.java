4bf55a92c843d862244199e5dd957ee9390603e0
LuizRossini <luiz.guirossini123@gmail.com>
2024-07-02 19:16:48 -0300

    pacote e classe NumerosPrimos

---
+++ src/Desafio04/NumerosPrimos.java
@@ -0,0 +1,8 @@
+/* 4 - Crie uma classe NumerosPrimos com métodos como verificarPrimalidade() e listarPrimos().
+Em seguida, crie duas subclasses, VerificadorPrimo e GeradorPrimo, que herdam da classe NumerosPrimos.
+Adicione um método específico para cada uma das subclasses, como verificarSeEhPrimo() para o VerificadorPrimo e
+gerarProximoPrimo() para o GeradorPrimo. */
+package Desafio04;
+
+public class NumerosPrimos {
+}
